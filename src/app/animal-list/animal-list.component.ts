2be2bccc37c8e98a11e48ef051de0b10ef7d839a
Max Scher <maxobaxo@gmail.com>
2017-08-18 09:10:58 -0700

    create animal-list component using hard-coded data

---
+++ src/app/animal-list/animal-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Animal } from './../animal.model';
+
+@Component({
+  selector: 'animal-list',
+  templateUrl: './animal-list.component.html',
+  styleUrls: ['./animal-list.component.css']
+})
+export class AnimalListComponent implements OnInit {
+  @Input() childAnimalList: Animal[];
+
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Animal } from './animal.model';
 
 @Component({
@@ -8,4 +8,11 @@ import { Animal } from './animal.model';
 })
 export class AppComponent {
   title = 'Wayward Pines Zoo: Animal Database';
+  masterAnimalList: Animal[] = [
+    new Animal('Lemur', 'JoJo', 5, 'Fruits & Veggies', 'Ape Terrarium', 1, 'F', 'Bananas', 'The Color Red'),
+    new Animal('Otter', 'Torrey', 2, 'Sea Urchin and Herring', 'Open Ocean Exhibit', 2, 'F', 'Burping', 'Tom from the Nocturnal Building'),
+    new Animal('Flamingo', 'Rick', 1, 'Algae & Brine Shrimp', 'Tropical Area', 1, 'M', 'Sleeping', 'Being Startled')
+  ]
+
+
 }
