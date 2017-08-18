e3caa45dfb05007f55f408879d7c6c23b27e4079
Max Scher <maxobaxo@gmail.com>
2017-08-18 08:37:37 -0700

    create animal model

---
+++ src/app/animal.ts
@@ -0,0 +1,3 @@
+export class Animal {
+  constructor(public name: string, public age: number, public diet: string, public location: string, public caretakersNeeded: number, public sex: string, public like: string, public dislike: string) { }
+}
